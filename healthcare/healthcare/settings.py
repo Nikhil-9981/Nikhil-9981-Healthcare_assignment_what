b627771b2c8babe706c1dfcdc8d89d13246224c1
Nikhil Kumar Singh <113182533+Nikhil-9981@users.noreply.github.com>
2025-05-04 16:18:43 +0530

    drf added in settings.py

--- healthcare/healthcare/settings.py
+++ healthcare/healthcare/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+     'drf_yasg',
     'rest_framework_simplejwt',
     'users', 'patients', 'doctors', 'mappings',
 ]
